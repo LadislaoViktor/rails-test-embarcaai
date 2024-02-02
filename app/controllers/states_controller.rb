0498fdb7eae77ccf41f3fe9b04a55b972b945635
LadislaoViktor <ladislaoviktor@gmail.com>
2024-02-01 23:42:12 -0300

    Ran rubocop.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,5 +1,5 @@
 class StatesController < ApplicationController
-  before_action :set_state, only: %i[ show edit update destroy ]
+  before_action :set_state, only: %i[show edit update destroy]
 
   # GET /states or /states.json
   def index
@@ -58,13 +58,14 @@ class StatesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_state
-      @state = State.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def state_params
-      params.require(:state).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_state
+    @state = State.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def state_params
+    params.require(:state).permit(:name)
+  end
 end
--- seeds_helper.rb
+++ seeds_helper.rb
@@ -3,31 +3,31 @@ require 'json'
 # Get each name:
 
 def hash_parser(json_file)
-    if json_file
-        data_hash = JSON.parse(json_file)
-    
-        last_name = data_hash.to_a.last['nome'].to_s
-         
-        data_hash.each do |n| 
-            unless last_name.eql?(n['nome'].to_s) 
-                puts n['nome'].to_s + ','
-            else 
-                puts n['nome'].to_s
-            end 
-        end
-    else
-        p 'deu ruim'
+  if json_file
+    data_hash = JSON.parse(json_file)
+
+    last_name = data_hash.to_a.last['nome'].to_s
+
+    data_hash.each do |n|
+      unless last_name.eql?(n['nome'].to_s)
+        puts n['nome'].to_s + ','
+      else
+        puts n['nome'].to_s
+      end
     end
+  else
+    p 'deu ruim'
+  end
 end
 
-p'Municípios do Paraná:'
+p 'Municípios do Paraná:'
 json_file = File.read('./municipios.json')
 hash_parser(json_file)
 
-p'Municípios de SC:'
+p 'Municípios de SC:'
 json_file = File.read('./municipios-sc.json')
 hash_parser(json_file)
 
-p'Municípios do Rio Grande do Sul:'
+p 'Municípios do Rio Grande do Sul:'
 json_file = File.read('./municipios-rs.json')
 hash_parser(json_file)
